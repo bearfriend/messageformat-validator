f51f339f5c68f667806e292744974151d90488f8
Danny Gleckler <daniel.gleckler@gmail.com>
2018-09-21 16:48:31 -0400

    0.0.10

--- test.js
+++ test.js
@@ -1,12 +1,21 @@
 'use strict';
 
-const requireDir = require('require-dir');
-const locales = requireDir('./test/locales/json'); // TODO: make dynamic
+const fs = require('fs');
+//const requireDir = require('require-dir');
+//const locales = requireDir('./test/locales/json/bigfiles'); // TODO: make dynamic
 const { validateLocales } = require('./src/validate');
 
+/*
 Object.keys(locales).forEach(key => {
   locales[key] = JSON.stringify(locales[key], null, 2);
 });
+*/
+
+const locales = {
+  en: fs.readFileSync('./test/locales/json/bigfiles/en.json', 'utf8'),
+  fr: fs.readFileSync('./test/locales/json/bigfiles/fr.json', 'utf8')
+}
+
 
 const issues = validateLocales({
   locales,
