3c25c0aec8fc933148e162937cc246f2b8f179e2
Danny Gleckler <daniel.gleckler@gmail.com>
2018-11-02 13:50:58 -0400

    Properly format CLI output

--- bin/cli.js
+++ bin/cli.js
@@ -6,12 +6,13 @@
 
 const fs = require('fs');
 const { promisify } = require('util');
+const chalk = require('chalk');
 const readFile = promisify(fs.readFile);
 const commander = require('commander');
 const findConfig = require('find-config');
 const configPath = findConfig('mfv.config.json');
 const { version } = require('../package.json');
-const { localesPath, sourceLocale } = require(configPath);
+const { path, source } = require(configPath);
 const { validateLocales } = require('../src/validate');
 
 commander
@@ -22,13 +23,15 @@ commander
   .option('-p, --path [path]', 'Path to a directory containing locale files')
   .parse(process.argv);
 
-const absLocalesPath = `${process.cwd()}/${commander.path || localesPath}`;
+
+const localesPath = path || commander.path;
+const absLocalesPath = `${process.cwd()}/${localesPath}`;
 
 fs.readdir(absLocalesPath, (err, files) => {
   if (err) console.log(`Failed to read ${absLocalesPath}`);
   let filteredFiles = files;
   if (commander.locales) {
-    commander.locales.push(sourceLocale);
+    commander.locales.push(source);
     filteredFiles = files.filter(file => commander.locales.includes(file.replace('.json', '')));
   }
   Promise.all(filteredFiles.map((file) => readFile(absLocalesPath + file, 'utf8')))
@@ -37,26 +40,37 @@ fs.readdir(absLocalesPath, (err, files) => {
       acc[filteredFiles[idx].replace('.json','')] = json;
       return acc;
     }, {});
+    const output = validateLocales({ locales, sourceLocale: source });
 
-    const output = validateLocales({ locales, sourceLocale });
+    output.forEach((locale, idx) => {
+      console.log((idx > 0 ? '\n' : '') + chalk.underline(`${absLocalesPath}${locale.locale}.json`));
+      if (commander.issues) {
+        locale.issues.forEach((issue) => {
+          console.log(
+            '  ' + chalk.grey(`${issue.line}:${issue.column}`) +
+            '  ' + chalk[issue.level == 'error' ? 'red' : 'yellow'](issue.level) +
+            ' ' + chalk.grey(issue.type) +
+            '  ' + chalk.cyan(issue.key) +
+            '  ' + chalk.white(issue.msg)
+          );
+        });
+      }
 
-    if (commander.issues) {
-      const json = JSON.stringify(output, null, 2);
-      console.log(json);
-    }
-    else {
-      const noIssuesOutput = output.map((locale) => {
-        const { issues, ...noIssues } = locale; // eslint-disable-line no-unused-vars
-        return noIssues;
-      });
-      const json = JSON.stringify(noIssuesOutput, null, 2);
-      console.log(json);
-    }
+      if (locale.report.totals.errors || locale.report.totals.warnings) {
+        const color = locale.report.totals.errors ? 'red' : 'yellow';
+        const total = locale.report.totals.errors + locale.report.totals.warnings;
+        const cliReport = chalk[color](`\n\u2716 ${total} issues (${locale.report.totals.errors} errors, ${locale.report.totals.warnings} warnings)`);
+        console.log(cliReport);
+      }
+      else {
+        const cliReport = `\n ${chalk.green('\u2714')} Passed`;
+        console.log(cliReport);
+      }
+    });
 
     if (commander.throwErrors && output.some((locale) => locale.report.totals.errors)) {
       throw new Error('Errors were reported in at least one locale. See details above.');
     }
-
   })
   .catch((errAll) => {
     console.error(errAll);
