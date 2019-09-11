edf2e30420f36b90018d5a4a80cc203bee86b4b0
Danny Gleckler <daniel.gleckler@gmail.com>
2019-09-11 12:39:41 -0400

    Add remove-extraneous subcommand

--- bin/cli.js
+++ bin/cli.js
@@ -22,8 +22,14 @@ commander
   .option('-l, --locales <items>', 'Process only these comma-separated locales', val => val.split(','))
   .option('-p, --path [path]', 'Path to a directory containing locale files')
   .option('-t, --translator-output', 'Output JSON of all source strings that are missing or untranslated in the target')
-  .parse(process.argv);
 
+commander
+  .command('remove-extraneous')
+  .action(() => {
+    commander.removeExtraneous = true;
+  })
+
+commander.parse(process.argv);
 
 const localesPath = path || commander.path;
 const absLocalesPath = `${process.cwd()}/${localesPath}`;
@@ -41,6 +47,10 @@ fs.readdir(absLocalesPath, (err, files) => {
       return acc;
     }, {});
 
+    if (commander.removeExtraneous) {
+      console.log('Removing extraneous strings from:', filteredFiles.join(', '));
+    }
+
     const output = validateLocales({ locales, sourceLocale: source });
     const translatorOutput = {};
 
@@ -48,8 +58,17 @@ fs.readdir(absLocalesPath, (err, files) => {
       if (!commander.locales || commander.locales.includes(locale.locale)) {
         console.log((idx > 0 ? '\n' : '') + chalk.underline(`${absLocalesPath}${locale.locale}.json`));
         if (commander.issues) {
+
+          const localeStrings = JSON.parse(locales[locale.locale]);
           locale.issues.forEach((issue) => {
-            if (commander.translatorOutput) {
+            if (commander.removeExtraneous) {
+              if (issue.type === 'extraneous') {
+                delete localeStrings[issue.key];
+                console.log('Removed:', issue.key);
+              }
+
+            }
+            else if (commander.translatorOutput) {
               if (['missing', 'untranslated'].includes(issue.type)) {
                 translatorOutput[issue.key] = issue.source;
               }
@@ -64,13 +83,23 @@ fs.readdir(absLocalesPath, (err, files) => {
               );
             }
           });
+
+          if (commander.removeExtraneous) {
+            const localeJSON = JSON.stringify(localeStrings, null, 3).replace(/": "/g, '" : "');
+            fs.writeFileSync(absLocalesPath + locale.locale + '.json', localeJSON + '\n');
+          }
         }
 
         if (commander.translatorOutput) {
           console.log(JSON.stringify(translatorOutput, null, 2));
         }
 
-        if (locale.report.totals.errors || locale.report.totals.warnings) {
+        if (commander.removeExtraneous) {
+          const count = locale.report.errors ? locale.report.errors.extraneous || 0 : 0;
+          const cliReport = `\n ${chalk.green('\u2714')} Removed ${count} extraneous strings`;
+          console.log(cliReport);
+        }
+        else if (locale.report.totals.errors || locale.report.totals.warnings) {
           const color = locale.report.totals.errors ? 'red' : 'yellow';
           const total = locale.report.totals.errors + locale.report.totals.warnings;
           const cliReport = chalk[color](`\n\u2716 ${total} issues (${locale.report.totals.errors} errors, ${locale.report.totals.warnings} warnings)`);
