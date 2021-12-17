6a481339b6f10a329dc1a18f41d4d61e015998cc
Danny Gleckler <daniel.gleckler@gmail.com>
2021-12-17 16:25:50 -0500

    Fix nesting order check (#15)
    * Fix nesting order check
    
    * 2.0.5

--- src/validate.js
+++ src/validate.js
@@ -137,7 +137,10 @@ function validateString({ targetString, targetLocale, sourceString, sourceLocale
       }
     }
 
-    if (targetMap.cases.findIndex(c => c[0] !== '|select|') > 0) {
+    const hasPlural = targetMap.cases.find(c => c.startsWith('|plural|'));
+    const lastItem = targetMap.cases[targetMap.cases.length - 1];
+
+    if (hasPlural && !lastItem.startsWith('|plural|')) {
       reporter.warning('nest', 'Plurals should always nest inside selects.');
     }
 
